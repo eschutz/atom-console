83f2930af2aa9271ef4a75a6fd6af76aef58cdf1
Elijah Schutz <elijah@jschutz.net>
2016-10-06 23:29:03 +1030

    first commit

---
+++ spec/atom-console-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomConsoleView from '../lib/atom-console-view';
+
+describe('AtomConsoleView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
